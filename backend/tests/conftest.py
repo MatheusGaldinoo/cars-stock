d3686866bb5d120ae1012f8a0184d269f2cf6a27
MatheusGaldinoo <matheus.galdino.souza@ccc.ufcg.edu.br>
2026-04-01 21:50:17 -0300

    refact: change conftest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock, patch
 from fastapi.testclient import TestClient
 
 from src.main import app
+from src.repositories.database import get_db
 
 @pytest.fixture(scope="function")
 def mock_conn():
@@ -11,10 +12,9 @@ def mock_conn():
 
 @pytest.fixture(scope="function")
 def client(mock_conn):
-    with patch('src.repositories.database.get_connection', return_value=mock_conn):
-        with patch('src.repositories.database.release_connection'):
-            with TestClient(app) as c:
-                yield c
+    with patch('src.repositories.database.get_db', return_value=mock_conn):
+        with TestClient(app) as c:
+            yield c
 
 @pytest.fixture(scope="function")
 def mock_cursor(mock_conn):
